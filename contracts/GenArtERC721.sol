4e283042a82cdfc33fd8d500b2f55a6c8f134091
stzky <chris@gen.art>
2022-03-20 12:06:29 +0100

    update comments

--- contracts/GenArtERC721.sol
+++ contracts/GenArtERC721.sol
@@ -12,6 +12,12 @@ import "./IGenArtInterfaceV2.sol";
 
 /**
  * @dev GEN.ART ERC721 V1
+ * Implements the extentions {IERC721Enumerable} and {IERC2981}.
+ * Inherits access control from {GenArtAccess}.
+ * Sends all ETH to a {PaymentSplitter} contract.
+ * Restricts minting to GEN.ART Membership holders.
+ * IMPORTANT: This implementation requires the royalties to be send to the contracts address
+ * in order to split the fund between payees automatically.
  */
 contract GenArtERC721 is GenArtAccess, ERC721Enumerable, IERC2981 {
     using Strings for uint256;
@@ -35,6 +41,9 @@ contract GenArtERC721 is GenArtAccess, ERC721Enumerable, IERC2981 {
 
     MintStates.State public _mintstate;
 
+    /**
+     *@dev Emitted in mint
+     */
     event Mint(
         uint256 tokenId,
         uint256 collectionId,
@@ -71,6 +80,9 @@ contract GenArtERC721 is GenArtAccess, ERC721Enumerable, IERC2981 {
             super.supportsInterface(interfaceId);
     }
 
+    /**
+     *@dev Get available mints for an account
+     */
     function getAvailableMintsForAccount(address account)
         public
         view
@@ -93,6 +105,9 @@ contract GenArtERC721 is GenArtAccess, ERC721Enumerable, IERC2981 {
         return availableMints;
     }
 
+    /**
+     *@dev Get available mints for a membershipId
+     */
     function getAvailableMintsForMembership(uint256 membershipId)
         public
         view
@@ -107,6 +122,10 @@ contract GenArtERC721 is GenArtAccess, ERC721Enumerable, IERC2981 {
             );
     }
 
+    /**
+     *@dev Check if minter has available mint slots and has sent the required amount of ETH
+     * Revert in case minting is paused or checks fail.
+     */
     function checkMint(uint256 amount, uint256 availableMints) internal view {
         require(!_paused, "GenArtERC721: minting is paused");
         require(availableMints > 0, "GenArtERC721: no mints available");
@@ -124,53 +143,75 @@ contract GenArtERC721 is GenArtAccess, ERC721Enumerable, IERC2981 {
         );
     }
 
+    /**
+     *@dev Public function to mint the desired amount of tokens
+     * Requirments:
+     * - sender must be GEN.ART Membership owner
+     */
     function mint(address to, uint256 amount) public payable {
+        // get all available mints for sender
         uint256 availableMints = getAvailableMintsForAccount(_msgSender());
         checkMint(amount, availableMints);
+        // get all memberships for sender
         uint256[] memory memberships = IGenArtMembership(_genartMembership)
             .getTokensByOwner(_msgSender());
         uint256 minted;
         uint256 i;
+
+        // loop until the desired amount of tokens was minted
         while (minted < amount && i < memberships.length) {
+            // check if membership is gold
             bool isGold = IGenArtInterfaceV2(_genartInterface).isGoldToken(
                 memberships[i]
             );
+            // get available mints for membership
             uint256 mints = _mintstate.getAvailableMints(
                 memberships[i],
                 isGold,
                 MINT_SUPPLY,
                 totalSupply()
             );
+            // mint tokens with membership and stop if desired amount reached
             for (uint256 j; j < mints && minted < amount; j++) {
                 mintForMembership(to, memberships[i], isGold);
                 minted++;
             }
             i++;
         }
+        // send funds to PaymentSplitter
         IGenArtPaymentSplitter(_paymentSplitter).splitPayment(address(this));
     }
 
     function mintOne(address to, uint256 membershipId) public payable {
+        // check if sender is owner of membership
         require(
             IGenArtInterfaceV2(_genartInterface).ownerOf(membershipId) ==
                 _msgSender(),
             "GenArtERC721: sender is not membership owner"
         );
+        // get available mints for membership
         uint256 availableMints = getAvailableMintsForMembership(membershipId);
+
         checkMint(1, availableMints);
+        // mint token
         mintForMembership(
             to,
             membershipId,
             IGenArtInterfaceV2(_genartInterface).isGoldToken(membershipId)
         );
+        // send funds to PaymentSplitter
         IGenArtPaymentSplitter(_paymentSplitter).splitPayment(address(this));
     }
 
+    /**
+     *@dev Mint token for membership
+     */
     function mintForMembership(
         address to,
         uint256 membershipId,
         bool isGold
     ) internal {
+        // update mint state once membership minted a token
         _mintstate.update(membershipId, isGold, 1);
         _mintOne(to, membershipId);
     }
@@ -193,25 +234,35 @@ contract GenArtERC721 is GenArtAccess, ERC721Enumerable, IERC2981 {
         emit Mint(tokenId, _collectionId, membershipId, to);
     }
 
+    /**
+     *@dev Reserved mints can only be called by admins
+     * Only one possible mint.
+     */
     function reservedMint(address to) public payable onlyAdmin {
+        require(!_reservedMinted, "GenArtERC721: reserved already minted");
         _mintOne(to, 0);
         _reservedMinted = true;
     }
 
     function burn(uint256 tokenId) public {
+        // check if sender is owner of token
+        address owner = ERC721.ownerOf(tokenId);
         require(
-            _isApprovedOrOwner(_msgSender(), tokenId),
-            "GenArtERC721: burn caller is not owner nor approved"
+            _msgSender() == owner,
+            "GenArtERC721: burn caller is not owner"
         );
         _burn(tokenId);
     }
 
+    /**
+     *@dev Pause and unpause minting
+     */
     function setPaused(bool paused) public onlyAdmin {
         _paused = paused;
     }
 
     /**
-     * @dev Get royalty info see IERC2981
+     * @dev Get collection info
      */
     function getCollectionInfo()
         public
@@ -236,7 +287,7 @@ contract GenArtERC721 is GenArtAccess, ERC721Enumerable, IERC2981 {
     }
 
     /**
-     * @dev Get royalty info see IERC2981
+     * @dev Get royalty info see {IERC2981}
      */
     function royaltyInfo(uint256, uint256 salePrice_)
         external
@@ -276,7 +327,7 @@ contract GenArtERC721 is GenArtAccess, ERC721Enumerable, IERC2981 {
     }
 
     /**
-     *@dev Royalties forwarded to PaymentSplitter
+     *@dev Royalties are forwarded to {PaymentSplitter}
      */
     receive() external payable {
         IGenArtPaymentSplitter(_paymentSplitter).splitPaymentRoyalty(
