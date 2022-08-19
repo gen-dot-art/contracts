b6c02498b05e6dfe3fb9b982252b50ed24407861
stzky <chris@gen.art>
2022-08-20 00:16:21 +0200

    remove imports

--- contracts/GenArtSharing.sol
+++ contracts/GenArtSharing.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.0;
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./GenArtAccess.sol";
-import "./IGenArtInterfaceV3.sol";
-import "./GenArtDutchAuctionHouse.sol";
 
 /**
  * @title GenArtSharing
